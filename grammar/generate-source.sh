5255453108df89f2fe5cbe9e37a11d324c7257e6
gwendallg <gwendall.gannier.fr@gmail.com>
2022-11-01 18:19:58 +0100

    update ci

--- grammar/generate-source.sh
+++ grammar/generate-source.sh
@@ -2,6 +2,6 @@
 echo '--------------------------------------------'
 echo ' source generation C# - rsql                '
 echo '--------------------------------------------'
-java -jar antlr-4.6-complete.jar RSqlQuery.g -o ../src/RSql4net/Models/Queries -Dlanguage=CSharp -listener -encoding UTF-8 -visitor -package RSql4Net.Models.Queries
+java -jar antlr4-4.9.3-complete.jar RSqlQuery.g -o ../src/RSql4net/Models/Queries -Dlanguage=CSharp -listener -encoding UTF-8 -visitor -package RSql4Net.Models.Queries
 
 
